595b590a103ac2f1d61440a40de2474f9277c3e8
adoniranfranceh <adoniranfranca@gmail.com>
2024-04-11 18:03:04 -0300

    Adiciona possibilidade de dica para o usuário

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -31,6 +31,16 @@ const Question = () => {
             selectOption={() => onSelectOption(option)}
           />
         ))}
+        {!quizState.answerSelected && !quizState.help && (
+          <>
+            {currentQuestion.tip && (
+              <button onClick={() => dispatch({ type: 'SHOW_TIP' }) }>Dica</button>
+            )}
+          </>
+        )}
+        {!quizState.answerSelected && quizState.help == 'tip' && (
+          <p>{currentQuestion.tip}</p>
+        )}
         {quizState.answerSelected && (
           <button onClick={ () => dispatch({ type: 'CHANGE_QUESTION' }) }>
             Continuar
